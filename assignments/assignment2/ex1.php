580d7f96e4042e6604bb07ad832351a264ce4ca1
Rosalie Klein <74024198+RosalieKlein8@users.noreply.github.com>
2021-10-04 22:56:44 -0400

    Update ex1.php

--- assignments/assignment2/ex1.php
+++ assignments/assignment2/ex1.php
@@ -10,9 +10,9 @@ $output = "";
         }
 
         $output .= "</ul></ul>";
-       
+        
     }
-    
+
 ?>
 
 <!doctype html>
@@ -25,7 +25,7 @@ $output = "";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Assignment 2</title>
+    <title>Assignment 2 Exercise 1</title>
 
 </head>
 
