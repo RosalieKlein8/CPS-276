23b578b8f0d501f4d2bfa3630e02a26229da3539
Rosalie Klein <74024198+RosalieKlein8@users.noreply.github.com>
2021-10-04 22:19:42 -0400

    Update ex1.php

--- assignments/assignment2/ex1.php
+++ assignments/assignment2/ex1.php
@@ -1,1 +1,39 @@
+<?php
 
+$output = "";    
+
+    for ($i = 1; $i < 5; $i++){
+        $output .= "<ul><li>$i</li><ul>";
+        
+        for ($j = 1; $j < 6; $j++){
+            $output .= "<li>$j</li>";
+        }
+
+        $output .= "</ul></ul>";
+        
+    }
+    
+
+?>
+
+<!doctype html>
+<html lang="en">
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Assignment 2</title>
+
+</head>
+
+<body>
+
+      <?php echo $output ?>
+
+</body>
+
+</html>
