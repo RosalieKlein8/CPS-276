5c8cc660cc3f70d4eac4ed02fc565353f0404cc4
Rosalie Klein <74024198+RosalieKlein8@users.noreply.github.com>
2021-10-04 23:47:00 -0400

    Update ex3.php

--- assignments/assignment2/ex3.php
+++ assignments/assignment2/ex3.php
@@ -1,1 +1,42 @@
+<?php
+    
+    $rows = 15;
+    $cells = 5;
 
+    $table= "<table border= '1'>";
+
+    for($i = 1; $i <= $rows; $i++) {
+        $table = $table."<tr>";
+
+        for($j = 1; $j <= $cells; $j++) {
+            $table=$table."<td> Row ".$i." Cell ".$j."</td>";
+        }
+    }
+
+    $table = $table."</tr>";
+    $table = $table."</table>";
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Assignment 2 Exercise 3</title>
+
+</head>
+
+<body>
+
+    <?php echo $table ?>
+
+</body>
+
+</html>
