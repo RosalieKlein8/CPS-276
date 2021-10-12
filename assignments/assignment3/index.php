dab1ad239d9ea03e03b26c8530009a38a4239ce9
Rosalie Klein <74024198+RosalieKlein8@users.noreply.github.com>
2021-10-11 20:37:51 -0400

    Create index.php

---
+++ assignments/assignment3/index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Assignment 3</title>
+
+</head>
+
+<body>
+
+<?php 
+
+require_once "Calculator.php"; 
+$Calculator = new Calculator(); 
+echo $Calculator->calc("/", 10, 0); 
+echo $Calculator->calc("*", 10, 2);  
+echo $Calculator->calc("/", 10, 2); 
+echo $Calculator->calc("-", 10, 2);  
+echo $Calculator->calc("+", 10, 2); 
+echo $Calculator->calc("*", 10);
+echo $Calculator->calc(10); 
+
+?>
+
+</body>
+
+</html>
